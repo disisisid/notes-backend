b7a10a3f3fdb4358b21bb2005a4618eeddf5825c
disisisid <sidnshady@gmail.com>
2020-08-16 11:28:35 +0530

    db config

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import psycopg2
 from flask import Flask
+import urllib.parse as urlparse
+import os
 
-conn = psycopg2.connect(database="test", user = "sid", password = "", host = "127.0.0.1", port = "5432")
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
+
+#conn = psycopg2.connect(database="test", user = "sid", password = "", host = "127.0.0.1", port = "5432")
+conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+            )
+
 print("Opened database successfully")
 cur = conn.cursor()
 
